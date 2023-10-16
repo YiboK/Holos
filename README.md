e83ba709b2fed877ef86b7f170f92d44bb1bf6c0
Yuhan <458598322@qq.com>
2023-10-16 13:39:22 -0500

    Add testers for basic functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Holos
+
+Run "python3 -m backend.tester.backend_tester" to test backend
--- src/backend/tester/backend_tester.py
+++ src/backend/tester/backend_tester.py
@@ -1,9 +1,18 @@
-from util import chatgpt
+from ..util import chatgpt
 
 
 def test():
     result = chatgpt.generateSchedule("Say hello")
     assert "Hello" in result or "hello" in result
+
+    sentence = """
+    I would like to study computer science. 
+    I currently have no knowledge in this area. 
+    There are some specific terms I would like to learn, such as binary tree. 
+    I would like the study program to include no assignments, and I can dedicate 12 weeks to completing it. 
+    """
+    result = chatgpt.generateSchedule(sentence)
+    assert "binary tree" in result or "Binary tree" in result
 
 if __name__ == "__main__":
     try:
